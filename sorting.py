f2dbf59791e9d1a42ad495756d9e0ccd11f13cf9
dushyant joshi <dushyantjoshi@djdj.local>
2025-01-18 00:07:22 +0530

    Updated selection sort implementation

--- sorting.py
+++ sorting.py
@@ -1,1 +1,12 @@
-print("hi")
+class Solution:
+    def select(self, arr, i):
+        n = len(arr)
+        for i in range(0, n):
+            min_index = i
+            for j in range(i + 1, n):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    def selectionSort(self, arr, n):
+        self.select(arr, 0)
